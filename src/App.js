c04cf648f4812e509af4591c9072ae76d488e7bb
Miguel Angel <miguiperlop7@gmail.com>
2020-06-10 10:19:40 +0200

    Seccion 10 terminada

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React, { Component} from 'react';
+import Paper from '@material-ui/core/Paper';
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
+import Toolbar from '@material-ui/core/Toolbar';
+import { Grid, Col, Row } from 'react-flexbox-grid';
 import LocationList from './components/LocationList';
+import ForecastExtended from './components/ForecastExtended';
 import './App.css';
 
 const cities = [
@@ -9,20 +15,56 @@ const cities = [
   'Ciudad de México,mx',
   'Madrid,es',
   'Lima,pe',
+  'Fabero,es',
 ];
 class App extends Component {
 
+  constructor() {
+    super();
+    this.state = {
+      city: null,
+    };
+  }
+
   handleSelectedLocation = city => {
+    this.setState({ city });
     console.log(`handleSelectedLocation ${city}`);
   }
 
   render() {
+    const { city } = this.state;
     return (
-      <div className="App">
-        <LocationList 
-          cities={cities} 
-          onSelectedLocation={this.handleSelectedLocation}></LocationList>
-      </div>
+      <Grid>
+        <Row>
+          <AppBar position='sticky'>
+            <Toolbar>
+              <Typography variant='title' color='inherit'>
+                Weather App
+              </Typography>
+            </Toolbar>
+          </AppBar>
+        </Row>
+
+        <Row>
+
+          <Col xs={12} md={6}>
+            <LocationList 
+              cities={cities} 
+              onSelectedLocation={this.handleSelectedLocation}>
+            </LocationList>
+          </Col>
+          <Col xs={12} md={6}>
+            <Paper elevation={4}>
+              <div className="details">
+                {
+                  city && <ForecastExtended city={city}></ForecastExtended> 
+                }
+              </div>
+            </Paper>
+          </Col>
+        </Row>
+        
+      </Grid> 
     );
   }
 }
